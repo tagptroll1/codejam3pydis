cf1b73643dd74f0ff2183ab9f0c26082fb95f9fc
Guillaume Troupel <guillaume.troupel@gmail.com>
2018-10-25 00:19:21 +0200

    river flow less chaotic

--- project/maps/map.py
+++ project/maps/map.py
@@ -41,13 +41,16 @@ class Map:
         self.terrain = [[None] * n for _ in range(m)]
         self.initialize_base_terrain(Grass)
         self.initialize_trees(randrange((n * m) / 2))
-        self.initialize_rivers(randrange(n / 2), 1)
+        self.initialize_rivers(randrange(4), 1)
 
     def __repr__(self):
         return repr(self.terrain)
 
     def __str__(self):
         return '\n'.join(''.join(str(parcel) for parcel in line) for line in self.terrain)
+
+    def get_parcel(self, x, y):
+        return self.terrain[y][x]
 
     def initialize_parcel(self, x, y, _type, **parcel_kwargs):
         self.terrain[y][x] = _type(**parcel_kwargs)
@@ -64,7 +67,7 @@ class Map:
     def initialize_rivers(self, number, max_width):
         """initate rivers"""
         for _ in range(number):
-            start_point = (randrange(0, self.n), randrange(0, self.m))
+            start_point = (randrange(1, self.n - 1), randrange(1, self.m - 1))
             self.generate_river(start_point, 1)
 
     def initialize_trees(self, number):
@@ -91,9 +94,22 @@ class Map:
         dir_index = randrange(0, 8)
         new_point = np.array(start_point) + directions[dir_index]
         self.initialize_parcel(*new_point.tolist(), Water)
+        previous_dir_index = dir_index
 
+        #TODO an inifinite loop can occure because if no path il allowed. For instance a river trapped inside another one
         # draw the river until it hits a border
         while new_point[0] not in (0, self.n - 1) and new_point[1] not in (0, self.m - 1):
-            dir_index = np.clip(ceil(gauss(dir_index, .1)), 0, 7)
+            dir_index = int(np.clip(round(gauss(previous_dir_index, 1)), 0, 7))
+
+            # no going backwards
+            if abs(dir_index - previous_dir_index) == 4:
+                continue
+
             new_point += directions[dir_index]
+
+            # no colision with a existing river or itself
+            if isinstance(self.get_parcel(*new_point), Water):
+                continue
+
             self.initialize_parcel(*new_point.tolist(), Water)
+            previous_dir_index = dir_index
