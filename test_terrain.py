43e743a545d4bc3cfdc86df2d5e5715545b47f29
Guillaume Troupel <guillaume.troupel@gmail.com>
2018-10-25 00:31:46 +0200

    really fix conflict this time...

---
+++ test_terrain.py
@@ -0,0 +1,18 @@
+from project.maps.map import *
+
+
+if __name__ == '__main__':
+	my_map = Map(24,140)
+	print(my_map)
+
+#	print('\n* setting water base terrain')
+#	my_map.initialize_base_terrain(Water)
+#	print(my_map)
+#
+#	print('\n* setting grass base terrain')
+#	my_map.initialize_base_terrain(Grass)
+#	print(my_map)
+#
+#	print('\n* adding rivers')
+#	my_map.initialize_rivers(1, 1)
+#	print(my_map)
